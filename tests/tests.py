be1338b7e870ed38f9d69d75244a07fccce7df31
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2017-07-26 23:58:37 -0400

    Another bit of testing.

--- tests/tests.py
+++ tests/tests.py
@@ -18,3 +18,25 @@ class TestCatalogAPI(unittest.TestCase):
         assert len(result) > 1000
         # Assert the resource IDs are fully unique.
         assert len(result) == len({r['resource']['id'] for r in result})
+
+
+class TestJSONEndpointAPI(unittest.TestCase):
+    """
+    Tests that the JSON endpoint works. This is a networked test against the New York City Open Data Portal.
+    """
+    def test(self):
+        result = pysocrata.get_endpoints_using_raw_json_emission("data.cityofnewyork.us")
+        assert len(result['dataset']) > 1000
+
+
+class TestGetDatasets(unittest.TestCase):
+    """
+    Tests that fetching datasets, the module 'master method', works as expected.
+    """
+    def test(self):
+        with open("auth/nyc-open-data.json", "r") as f:
+            auth = json.load(f)
+        import pdb; pdb.set_trace()
+        result = pysocrata.get_datasets(**auth)
+        import pdb; pdb.set_trace()
+        pass
